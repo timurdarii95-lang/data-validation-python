a1cfc97d2358a5185414bc15bb492da0794f94f7
Timur Darii <timurdarii95@gmail.com>
2026-03-15 13:39:50 +0000

    feat: add cours module in package timur

--- timur/courses.py
+++ timur/courses.py
@@ -7,3 +7,18 @@ COURSES = []
 @app.get('/courses')
 async def read_all_courses():
     return COURSES
+
+class Course:
+    id:              int
+    title:           str
+    trainer:         str
+    description:     str
+    duration_weeks:  str
+
+def __init__(self, id, title, trainer, description, duration_weeks):
+    self.id             = id
+    self.title          = title
+    self.trainer        = trainer
+    self.descption      = description
+    self.duration_weeks = duration_weeks
+
