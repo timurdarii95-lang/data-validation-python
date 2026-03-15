dca63527e60b6ea72417eedb8e7b5aedb6e6be78
Sorin Gore <soringoredev@gmail.com>
2026-03-15 16:02:45 +0200

    Add course structure data

--- sorin/courses.py
+++ sorin/courses.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-COURSES = []
+# COURSES = []
 
 @app.get('/courses')
 async def read_all_courses():
@@ -22,3 +22,39 @@ class Course:
         self.trainer = trainer
         self.description = description
         self.duration_weeks = duration_weeks
+
+COURSES = [
+    Course(1,
+           'Mathematics',
+           'John Smith',
+           'Fundamentals of math',
+           4
+    ),
+    Course(2,
+           'Computer Science',
+           'Marry Poppins',
+           'Fundamentals of computer science',
+           8
+    ),
+    Course(3,
+           'Java Backend',
+           'Martin Morgan',
+           'OOP, variables and loops',
+           3
+    ),
+    Course(4,
+           'Python',
+           'Andrew Louder',
+           'Python & Python Pro',
+           5
+    ),
+    Course(5,
+           'Databases',
+           'Mark Michigan',
+           'Lorem ipsum description',
+           5
+    )
+
+]
+
+
