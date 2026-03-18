f535527312e11baf08a1bbacce39d53df313936d
Timur Darii <timurdarii95@gmail.com>
2026-03-18 20:17:42 +0000

    feat: pydentic_work

--- timur/courses.py
+++ timur/courses.py
@@ -1,11 +1,25 @@
 from email.policy import default
-from pydantic import BaseModel
+from typing import Optional
 
 from fastapi import FastAPI, Body
-from pydantic import BaseModel
-from sorin.courses import Course
+from pydantic import BaseModel, Field
+
 
 app = FastAPI()
+
+class Course:
+    id:              int
+    title:           str
+    trainer:         str
+    description:     str
+    duration_weeks:  int
+
+    def __init__(self, id, title, trainer, description, duration_weeks):
+        self.id = id
+        self.title = title
+        self.trainer = trainer
+        self.description = description
+        self.duration_weeks = duration_weeks
 
 COURSES = [
     Course(1,
@@ -41,30 +55,31 @@ COURSES = [
 
 ]
 class CourseRequest(BaseModel):
-    id:              int
-    title:           str
-    trainer:         str
-    description:     str
-    duration_weeks:  str
+    id:              Optional [int] = Field(description='ID is not needed to be created', default=None)
+    title:           str = Field(description='ciuvac minimum 3',min_length=3)
+    trainer:         str = Field(min_length=1)
+    description:     str = Field(min_length=1, max_length=100)
+    duration_weeks:  int = Field(gt=0,lt=6)
+
+def find_course_id(course: Course):
+    if len(COURSES) > 0:
+        course.id = COURSES[-1].id + 1
+    else:
+        course.id = 1
+
+    return course
 
 @app.get('/courses')
 async def read_all_courses():
     return COURSES
 
-class Course:
-    id:              int
-    title:           str
-    trainer:         str
-    description:     str
-    duration_weeks:  str
 
-def __init__(self, id, title, trainer, description, duration_weeks):
-    self.id             = id
-    self.title          = title
-    self.trainer        = trainer
-    self.description      = description
-    self.duration_weeks = duration_weeks
+
+
 
 @app.post('/create-course')
-async  def create_course(course_request: CourseRequest):
-    COURSES.append(course_request)
+async def create_course(course_request: CourseRequest):
+    new_course = Course(**course_request.model_dump())
+    print(type(new_course))
+    COURSES.append(new_course)
+
