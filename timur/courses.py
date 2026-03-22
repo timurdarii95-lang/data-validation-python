7c3324dba8c4737eb9b13613407c7ab242a0ac2b
Timur Darii <timurdarii95@gmail.com>
2026-03-22 17:11:29 +0000

    feat:add curses updated

--- timur/courses.py
+++ timur/courses.py
@@ -1,8 +1,10 @@
 from email.policy import default
 from typing import Optional
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException, Path, Query
+
 from pydantic import BaseModel, Field
+from starlette import status
 
 
 app = FastAPI()
@@ -13,44 +15,54 @@ class Course:
     trainer:         str
     description:     str
     duration_weeks:  int
+    start_year:      int
 
-    def __init__(self, id, title, trainer, description, duration_weeks):
+    def __init__(self, id, title, trainer, description, duration_weeks, start_year):
         self.id = id
         self.title = title
         self.trainer = trainer
         self.description = description
         self.duration_weeks = duration_weeks
+        self.start_year = start_year
 
 COURSES = [
     Course(1,
            'Matematics',
            'Jhon Smith',
            'Fundamentals of maths',
-           4
+           4,
+           2020
+
     ),
     Course(5,
            'Computer since',
            'Marry Poppins',
            'Fundamental of computer since',
-           8
+           8,
+           2019
     ),
     Course(3,
            'Java Backend',
            'Martin Morgan',
            'OOP, variables and loops',
-           3
+           3,
+           2015
            ),
     Course(4,
            'Python',
            'Andrew Louder',
            'Python & Python Pro',
-           5
+           5,
+           2010
            ),
-    Course(5,
+    Course(7,
            'Databases',
            'Mark Michigan',
            'Lorem ipsum description',
-           5
+           5,
+           2023
+
+
            )
 
 ]
@@ -60,6 +72,20 @@ class CourseRequest(BaseModel):
     trainer:         str = Field(min_length=1)
     description:     str = Field(min_length=1, max_length=100)
     duration_weeks:  int = Field(gt=0,lt=6)
+    start_year: int = Field(gt= 2020, lt=2031)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "Titel of a new car",
+                "trainer": "Martin Morgan",
+                "description": "A new description of a course",
+                "duration_weeks":4,
+                "start_year": 2026
+
+            }
+        }
+    }
 
 def find_course_id(course: Course):
     if len(COURSES) > 0:
@@ -69,17 +95,56 @@ def find_course_id(course: Course):
 
     return course
 
-@app.get('/courses')
+@app.get('/courses', status_code=status.HTTP_200_OK)
 async def read_all_courses():
     return COURSES
 
+@app.get('/courses/{course_id}')
+async  def read_course(course_id: int = Path(gt=0)):
+    for course in COURSES:
+        if course.id == course_id:
+            return course
+    raise HTTPException(status_code=404, detail='nihuia')
 
+@app.get('/courses/', status_code=status.HTTP_200_OK)
+async  def read_course_by_duration(course_duration: int = Query(gt=0, lt=6)):
+    courses_to_return = []
+    for course in COURSES:
+         if course.duration_weeks == course_duration:
+            courses_to_return.append(course)
+    return courses_to_return
 
-
-
-@app.post('/create-course')
+@app.post('/create-course', status_code=status.HTTP_201_CREATED)
 async def create_course(course_request: CourseRequest):
     new_course = Course(**course_request.model_dump())
     print(type(new_course))
     COURSES.append(new_course)
 
+@app.put('/courses/update_course', status_code=status.HTTP_204_NO_CONTENT)
+async def update_course(course: CourseRequest):
+    course_changed =False
+    for i in range (len(COURSES)):
+        if COURSES[i].id == course.id:
+            COURSES[i] = course
+            course_changed = True
+            if not course_changed:
+                raise  HTTPException(status_code=404, detail='nihuia')
+
+@app.delete('/courses/{course_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_course(course_id: int = Path(gt=0)):
+    course_changed = False
+    for i in range(len(COURSES)):
+        if COURSES[i].id == course_id:
+            COURSES.pop(i)
+            break
+            course_changed = True
+    if not course_changed:
+         raise HTTPException(status_code=404, detail='nihuia')
+
+@app.get('/courses/publish/', status_code=status.HTTP_200_OK)
+async def read_course_by_start_year(start_year: int = Query(gt=2020, lt=2031)):
+    course_to_return = []
+    for course in COURSES:
+        if course.start_year == start_year:
+            course_to_return.append(course)
+    return course_to_return
